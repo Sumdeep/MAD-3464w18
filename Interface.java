81f37899098c8cca90a289fb2005cb30ea26521d
Sumdeep <35268275+Sumdeep@users.noreply.github.com>
2018-03-15 11:46:24 -0400

    Add files via upload

---
+++ Interface.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkginterface;
+
+/**
+ *
+ * @author macstudent
+ */
+public class Interface {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+    }
+    
+}
+interface Person{
+    String name = "";
+    int age = 0;
+    int salary = 0;
+    String type = "";
+    
+    void display();
+}
+
+    
+   
+
